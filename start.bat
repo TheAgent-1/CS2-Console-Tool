1bee5e2cd6a5482dfbe265661f16bbbefabc388d
TheAgent-1 <108107690+TheAgent-1@users.noreply.github.com>
2026-01-11 13:04:02 +1300

    Update server directory paths in startup scripts
    Changed references from 'server' to 'cs2_server' in Start_CS2_Server.cmd and start.bat to reflect new server directory structure.

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-steamcmd\\steamcmd.exe +force_install_dir ../server/ +login anonymous +app_update 730 validate +quit
+steamcmd\\steamcmd.exe +force_install_dir ../cs2_server/ +login anonymous +app_update 730 validate +quit
 cd server\\game\\bin\\win64
 start /wait cs2.exe -dedicated -usercon -console -secure -dev +game_type 0 +game_mode 1 +sv_logfile 1 -serverlogging +sv_setsteamaccount 9B1A467603799448F18D7846544B71ED + sv_cheats 1 +map de_ancient +exec perf.cfg +exec bot_ai.cfg
