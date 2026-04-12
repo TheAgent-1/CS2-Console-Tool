53afae5527ebdb8d41e457fb163b5e03d25d8ae2
TheAgent-1 <108107690+TheAgent-1@users.noreply.github.com>
2026-04-12 14:51:38 +1200

    Wrong Fuckin Token Buddy

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 steamcmd\\steamcmd.exe +force_install_dir ../cs2_server/ +login anonymous +app_update 730 validate +quit
 cd server\\game\\bin\\win64
-start /wait cs2.exe -dedicated -usercon -console -secure -dev +game_type 0 +game_mode 1 +sv_logfile 1 -serverlogging +sv_setsteamaccount 9B1A467603799448F18D7846544B71ED + sv_cheats 1 +map de_ancient +exec perf.cfg +exec bot_ai.cfg
+start /wait cs2.exe -dedicated -usercon -console -secure -dev +game_type 0 +game_mode 1 +sv_logfile 1 -serverlogging +sv_setsteamaccount 392CDDAF8900FA00311EA404293006A7 + sv_cheats 1 +map de_ancient +exec perf.cfg +exec bot_ai.cfg
